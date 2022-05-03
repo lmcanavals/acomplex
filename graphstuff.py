5e07eca919cfbe9dd285f0390e302cd727072f7e
Luis M Canaval S <43156214+lmcanavals@users.noreply.github.com>
2022-05-02 19:29:07 -0500

    Create graphstuff.py

---
+++ graphstuff.py
@@ -0,0 +1,49 @@
+import graphviz as gv
+
+def adjlShow(L, labels=None, directed=False, weighted=False, path=[],
+             layout="sfdp"):
+  g = gv.Digraph("G") if directed else gv.Graph("G")
+  g.graph_attr["layout"] = layout
+  g.edge_attr["color"] = "gray"
+  g.node_attr["color"] = "orangered"
+  g.node_attr["width"] = "0.1"
+  g.node_attr["height"] = "0.1"
+  g.node_attr["fontsize"] = "8"
+  g.node_attr["fontcolor"] = "mediumslateblue"
+  g.node_attr["fontname"] = "monospace"
+  g.edge_attr["fontsize"] = "8"
+  g.edge_attr["fontname"] = "monospace"
+  n = len(L)
+  for u in range(n):
+    g.node(str(u), labels[u] if labels else str(u))
+  added = set()
+  for v, u in enumerate(path):
+    if u != None:
+      if weighted:
+        for vi, w in G[u]:
+          if vi == v:
+            break
+        g.edge(str(u), str(v), str(w), dir="forward", penwidth="2", color="orange")
+      else:
+        g.edge(str(u), str(v), dir="forward", penwidth="2", color="orange")
+      added.add(f"{u},{v}")
+      added.add(f"{v},{u}")
+  if weighted:
+    for u in range(n):
+      for v, w in L[u]:
+        if not directed and not f"{u},{v}" in added:
+          added.add(f"{u},{v}")
+          added.add(f"{v},{u}")
+          g.edge(str(u), str(v), str(w))
+        elif directed:
+          g.edge(str(u), str(v), str(w))
+  else:
+    for u in range(n):
+      for v in L[u]:
+        if not directed and not f"{u},{v}" in added:
+          added.add(f"{u},{v}")
+          added.add(f"{v},{u}")
+          g.edge(str(u), str(v))
+        elif directed:
+          g.edge(str(u), str(v))
+  return g
